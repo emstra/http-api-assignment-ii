7d49bad102dc27bed0fcf96198c34b2b5e62db0a
emstra <42919933+emstra@users.noreply.github.com>
2020-09-26 21:49:25 -0400

    Git commit api II

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const url = require('url');
+//const query = require('querystring');
+const jsonHandler = require('./jsonResponses');
+const htmlHandler = require('./htmlResponses');
+
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    // '/updateUser'   :   jsonHandler.updateUser,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
+  POST: {
+    '/addUser': jsonHandler.updateUser,
+  },
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  console.dir(parsedUrl.pathname);
+  console.dir(request.method);
+
+  // not perfect and will fail if HTTP method is not 'GET' or 'HEAD'
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response, parsedUrl);
+  } else {
+    urlStruct[request.method].notFound(request, response);
+  }
+};
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
